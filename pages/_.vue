6cadf9b1da24b9474018ec96c437a1036d18c89b
Azatyk <azatuk2005@mail.ru>
2021-05-04 11:53:12 +0600

    feat: add 404 page

---
+++ pages/_.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="not-found">
+    <img
+      src="@/static/images/not-found-image.svg"
+      alt="Страница не найдена"
+      class="not-found-image"
+    />
+    <h1 class="not-found-title">Упс... Страница не найдена</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.not-found {
+  padding-top: 70px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  &-image {
+    margin-bottom: 50px;
+    width: 300px;
+  }
+
+  &-title {
+    color: $main-dark;
+    font-size: 30px;
+  }
+}
+</style>
