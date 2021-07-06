134bfeaf2692f9e31712c458c2ca9198d7a0fed9
Azatyk <azatuk2005@mail.ru>
2021-07-06 17:29:55 +0600

    feat: add small images to service page

--- components/services/service-info/service-info.vue
+++ components/services/service-info/service-info.vue
@@ -4,10 +4,19 @@
       class="main-info__image"
       :style="{
         backgroundImage: `url(${
-          images.length > 0 ? baseUrl + images[0].path : defaultServiceImage
+          images.length > 0 ? baseUrl + activeImage : defaultServiceImage
         })`,
       }"
     ></div>
+    <div class="main-info__images">
+      <div
+        class="main-info__image-small"
+        v-for="image in images"
+        :key="image.id"
+        :style="{ backgroundImage: `url(${baseUrl + image.path})` }"
+        @click="activeImage = image.path"
+      ></div>
+    </div>
     <h1 class="main-info__title">{{ title }}</h1>
     <p class="main-info__description">{{ description }}</p>
   </div>
@@ -35,7 +44,14 @@ export default {
   data: () => ({
     baseUrl: config.apiUrl,
     defaultServiceImage: defaultServiceImage,
+    activeImage: '',
   }),
+
+  mounted() {
+    this.images.length > 0
+      ? (this.activeImage = this.images[0].path)
+      : (this.activeImage = this.defaultServiceImage)
+  },
 }
 </script>
 
@@ -47,11 +63,36 @@ export default {
 }
 
 .main-info__image {
-  margin: 0 0 20px 0;
+  margin-bottom: 10px;
   height: 270px;
   border-radius: 10px;
   background-size: cover;
   background-position: center top;
+  background-color: #dbdbdb;
+}
+
+.main-info__images {
+  margin-bottom: 10px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.main-info__image-small {
+  margin-right: 12px;
+  margin-bottom: 10px;
+  width: 60px;
+  height: 40px;
+  border-radius: 5px;
+  background-size: cover;
+  cursor: pointer;
+  transition: 200ms ease-in-out;
+  background-color: #dbdbdb;
+
+  &:hover {
+    opacity: 0.7;
+  }
 }
 
 .main-info__title {
