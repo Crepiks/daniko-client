a6e5c4c65bc4574193a0d7a5ec553492f748b885
Azatyk <azatuk2005@mail.ru>
2021-07-10 17:36:13 +0600

    feat: add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,11 @@ export default {
         href: 'https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css',
         rel: 'stylesheet',
       },
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: 'favicon.ico',
+      },
     ],
   },
 
